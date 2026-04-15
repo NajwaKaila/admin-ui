6094ed987aecebf74100b28dc2d6bde3fb045611
Najwa Kaila Nuraisyah <111202315379@mhs.dinus.ac.id>
2026-04-15 21:07:43 +0700

    membuat page sign in

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
