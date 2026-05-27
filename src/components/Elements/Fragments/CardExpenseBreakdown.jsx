3a3825500cae24a0b0dc140653752fbb6280d4e9
Najwa Kaila Nuraisyah <111202315379@mhs.dinus.ac.id>
2026-05-27 23:08:28 +0700

    state in component with hooks

---
+++ src/components/Elements/Fragments/CardExpenseBreakdown.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Card from "../Card";
+
+function CardExpenseBreakdown(props) {
+  const { data } = props;
+
+  return (
+    <>
+      <Card
+        title="Expenses Breakdown"
+        desc={
+          <div className="h-full md:grid md:grid-cols-3 gap-4">
+            {data.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center justify-between"
+              >
+                <div className="flex">
+                  <div>
+                    <div className="bg-special-bg text-gray-02 px-3 py-5 rounded-lg flex flex-col place-content-center">
+                      {item.icon}
+                    </div>
+                  </div>
+
+                  <div className="ms-4">
+                    <span className="text-gray-02">
+                      {item.category}
+                    </span>
+
+                    <br />
+
+                    <span className="font-bold text-lg">
+                      ${item.amount}
+                    </span>
+
+                    <div className="flex">
+                      <span className="text-gray-02">
+                        {item.percentage}%*
+                      </span>
+
+                      {" "}
+                      {item.arrow}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        }
+      />
+    </>
+  );
+}
+
+export default CardExpenseBreakdown;
--- src/components/Elements/Icon.jsx
+++ src/components/Elements/Icon.jsx
@@ -1,46 +1,54 @@
-import React from "react";
-import OverviewSVG from "../../assets/icons/Overview.svg";
-import TransactionSVG from "../../assets/icons/Transaction.svg";
-import BalanceSVG from "../../assets/icons/wallet.svg";
-import BillSVG from "../../assets/icons/Bill.svg";
-import ExpenseSVG from "../../assets/icons/Expencces.svg";
-import GoalSVG from "../../assets/icons/Goal.svg";
-import SettingSVG from "../../assets/icons/Settings.svg";
-import DetailSVG from "../../assets/icons/Icon.svg";
-import ChevronRightSVG from "../../assets/icons/chevrons-right.svg";
-import LogoutSVG from "../../assets/icons/Icon2.svg";
+import OverviewSVG from "../../assets/icons/Overview.svg?react";
+import TransactionSVG from "../../assets/icons/Transaction.svg?react";
+import BalanceSVG from "../../assets/icons/wallet.svg?react";
+import BillSVG from "../../assets/icons/Bill.svg?react";
+import ExpenseSVG from "../../assets/icons/Expencces.svg?react";
+import GoalSVG from "../../assets/icons/Goal.svg?react";
+import SettingSVG from "../../assets/icons/Settings.svg?react";
+import DetailSVG from "../../assets/icons/Icon.svg?react";
+import ChevronRightSVG from "../../assets/icons/chevrons-right.svg?react";
+import LogoutSVG from "../../assets/icons/Icon2.svg?react";
+import AdobeSVG from "../../assets/icons/Adobe.svg?react";
+import FigmaSVG from "../../assets/icons/Figma.svg?react";
+import FoodSVG from "../../assets/icons/Food.svg?react";
+import GamepadSVG from "../../assets/icons/gamepad-2.svg?react";
+import HouseSVG from "../../assets/icons/Housing.svg?react";
+import MovieSVG from "../../assets/icons/Movie.svg?react";
+import OtherSVG from "../../assets/icons/Others.svg?react";
+import ShoppingSVG from "../../assets/icons/Shopping.svg?react";
+import TransportSVG from "../../assets/icons/Trasnport.svg?react";
+import ArrowRightSVG from "../../assets/icons/arrow-right.svg?react";
+import ArrowDownSVG from "../../assets/icons/Downarrow.svg?react";
+import ArrowUpSVG from "../../assets/icons/Uparrow.svg?react";
+
+const createIcon =
+  (SVG) =>
+  ({ size = 24, color = "currentColor", ...props }) =>
+    <SVG width={size} height={size} stroke={color} {...props} />;
 
 const Icon = {
-  Overview: ({ size = 24, ...props }) => (
-    <img src={OverviewSVG} alt="Overview" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Transaction: ({ size = 24, ...props }) => (
-    <img src={TransactionSVG} alt="Transaction" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Balance: ({ size = 24, ...props }) => (
-    <img src={BalanceSVG} alt="Balance" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Bill: ({ size = 24, ...props }) => (
-    <img src={BillSVG} alt="Bill" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Expense: ({ size = 24, ...props }) => (
-    <img src={ExpenseSVG} alt="Expense" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Goal: ({ size = 24, ...props }) => (
-    <img src={GoalSVG} alt="Goal" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Setting: ({ size = 24, ...props }) => (
-    <img src={SettingSVG} alt="Setting" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Detail: ({ size = 24, ...props }) => (
-    <img src={DetailSVG} alt="Detail" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  ChevronRight: ({ size = 24, ...props }) => (
-    <img src={ChevronRightSVG} alt="ChevronRight" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
-  Logout: ({ size = 24, ...props }) => (
-    <img src={LogoutSVG} alt="Logout" style={{ width: size, height: size }} className="invert" {...props} />
-  ),
+  Overview: createIcon(OverviewSVG),
+  Transaction: createIcon(TransactionSVG),
+  Balance: createIcon(BalanceSVG),
+  Bill: createIcon(BillSVG),
+  Expense: createIcon(ExpenseSVG),
+  Goal: createIcon(GoalSVG),
+  Setting: createIcon(SettingSVG),
+  Detail: createIcon(DetailSVG),
+  ChevronRight: createIcon(ChevronRightSVG),
+  Logout: createIcon(LogoutSVG),
+  Adobe: createIcon(AdobeSVG),
+  Figma: createIcon(FigmaSVG),
+  Food: createIcon(FoodSVG),
+  Gamepad: createIcon(GamepadSVG),
+  House: createIcon(HouseSVG),
+  Movie: createIcon(MovieSVG),
+  Other: createIcon(OtherSVG),
+  Shopping: createIcon(ShoppingSVG),
+  Transport: createIcon(TransportSVG),
+  ArrowRight: createIcon(ArrowRightSVG),
+  ArrowDown: createIcon(ArrowDownSVG),
+  ArrowUp: createIcon(ArrowUpSVG),
 };
 
 export default Icon;
