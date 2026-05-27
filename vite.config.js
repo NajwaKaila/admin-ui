bc09ac690fbca854cd96dd11e6e2ac04b82cc0e8
Najwa Kaila Nuraisyah <111202315379@mhs.dinus.ac.id>
2026-05-27 18:33:30 +0700

    react icons (libabry & custom)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; 
-import svg from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
